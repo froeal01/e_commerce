9b7962eddeb66598908c4a313f71a24c9b5c267f
Alex Froelich <alexfroelich@Alexs-MacBook-Pro.local>
2014-02-13 21:42:18 -0500

    .

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,11 @@ Bundler.require(:default, Rails.env)
 module ECommerce
   class Application < Rails::Application
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif,
+                                  "honeyscript-semibold-webfont.ttf",
+                                 "honeyscript-semibold-webfont.eot",
+                                 "honeyscript-semibold-webfont.svg",
+                                 "honeyscript-semibold-webfont.woff")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
