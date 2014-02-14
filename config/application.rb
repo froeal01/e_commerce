c2734425ddc795d7717632094ce6835b8400b2e7
Alex Froelich <alexfroelich@Alexs-MacBook-Pro.local>
2014-02-13 23:16:41 -0500

    precompiled css

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ Bundler.require(:default, Rails.env)
 
 module ECommerce
   class Application < Rails::Application
+    config.assets.precompile << /\.(svg|eot|woff|ttf)$/
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
