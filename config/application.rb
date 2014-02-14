b53c5552ea8a18f4f66d0024ba73fce084ab6469
Alex Froelich <alexfroelich@Alexs-MacBook-Pro.local>
2014-02-13 21:56:49 -0500

    add

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module ECommerce
                                   "honeyscript-semibold-webfont.ttf",
                                  "honeyscript-semibold-webfont.eot",
                                  "honeyscript-semibold-webfont.svg",
-                                 "honeyscript-semibold-webfont.woff")
+                                 "honeyscript-semibold  -webfont.woff")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
