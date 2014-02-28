b3e3c37e11bd37a3ac1529ed6d6cd63ee0104d12
Alex Froelich <alexfroelich@Alexs-MacBook-Pro.local>
2014-02-28 12:52:10 -0500

    credit cards show on account page

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,10 +1,15 @@
 class AccountsController < ApplicationController
+	include AccountsHelper
 	before_filter :authenticate_user!
 	
 
 	def index
-
-
+		 customer_info = Stripe::Customer.retrieve(current_user.stripe_user_id)
+		 array_of_credit_card_ids = find_cards(customer_info)
+		 if array_of_credit_card_ids[0] != 0
+		 	@array_of_card_hash = retrieve_card_info(array_of_credit_card_ids, customer_info)
+		 end
 	end
+
 
 end
